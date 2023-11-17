606a3573035ac6f983d496bab5f3a3008621d1f6
Akshat Kumar <akshatsatyam52@gmail.com>
2023-11-17 20:26:27 +0530

    updated files

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,8 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt="Screenshots of the invoice image desktop version"
-                      />
+                        alt={`${invoice.name}'s profile picture`}                      />
                       <p>{invoice.name}</p>
                     </div>
                   </td>
